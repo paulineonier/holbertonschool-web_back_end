fc96c81c590f541c8dd7af30aa7b0ebfee976c86
paulineonier <pauline.onier@outlook.fr>
2024-08-16 12:01:23 +0200

    task 2

--- python_variable_annotations/2-floor.py
+++ python_variable_annotations/2-floor.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python3
+
 """
 This module provides a function to calculate the floor of a float.
 """
@@ -14,3 +15,4 @@ def floor(n: float) -> int:
         int: The floored value of the float.
     """
     return int(n // 1)
+
