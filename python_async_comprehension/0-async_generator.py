988366043c616010d23238c49004b7d2e2319f60
paulineonier <pauline.onier@outlook.fr>
2024-08-22 11:41:58 +0200

    task 0 corecction pycodestyle

--- python_async_comprehension/0-async_generator.py
+++ python_async_comprehension/0-async_generator.py
@@ -3,9 +3,11 @@
 This module contains an asynchronous generator that yields random numbers.
 """
 
+
 import asyncio
 import random
 from typing import Generator
+
 
 async def async_generator() -> Generator[float, None, None]:
     """
