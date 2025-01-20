9e5f070b4cf71b37017dc7bb63b9498a67699560
paulineonier <pauline.onier@outlook.fr>
2025-01-20 15:01:55 +0100

    task 1 correction

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -3,9 +3,8 @@ console.log('Welcome to Holberton School, what is your name?');
 process.stdin.on('data', (data) => {
   const input = data.toString().trim();
   console.log(`Your name is: ${input}`);
-  process.stdin.end();
-});
 
-process.on('exit', () => {
   console.log('This important software is now closing');
+  process.exit(0);
 });
+	
