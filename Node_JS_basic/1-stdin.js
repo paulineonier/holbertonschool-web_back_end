90ef22c9b09214fb86a8211aad397f8ec8ce4803
paulineonier <pauline.onier@outlook.fr>
2025-01-23 15:00:01 +0100

    task 1 correction again

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,14 +1,13 @@
-const readline = require('readline');
-
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout
-});
-
 console.log('Welcome to Holberton School, what is your name?');
 
-rl.question('', (name) => {
-  console.log(`Your name is: ${name}`);
-  console.log('This important software is now closing');
-  rl.close();
+process.stdin.setEncoding('utf8');
+
+process.stdin.on('readable', () => {
+  let input = process.stdin.read();
+  if (input !== null) {
+    input = input.trim();
+    console.log(`Your name is: ${input}`);
+    console.log('This important software is now closing');
+    process.exit(0);
+  }
 });
